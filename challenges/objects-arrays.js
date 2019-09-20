f56716801a55cbd1b30fae5b87fa75939fb35027
Mel Maki <melody.rackham@gmail.com>
2019-09-20 11:18:04 -0600

    Completed Sprint Project

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -149,7 +149,9 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 */
 
 const lowCaseAnimalNames = [];
-zooAnimals.map(animals => lowCaseAnimalNames.push(`${animals.animal_name.toLowerCase()}`)
+zooAnimals.map(function(animals) {
+  return lowCaseAnimalNames.push(`${animals.animal_name.toLowerCase()}`);
+});
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter()
